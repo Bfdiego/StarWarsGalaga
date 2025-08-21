5d033e6a77e277f8183085d8902ed3def60a2c01
Santiago Acha <santiagoacha10@gmail.com>
2025-08-20 22:52:01 -0400

    ASSES

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -2,7 +2,7 @@ import arcade
 from settings import ENEMY_SCALING
 
 class Enemy(arcade.Sprite):
-    def __init__(self, image="assets/player.png", scale=ENEMY_SCALING):
+    def __init__(self, image="assets/enemy.png", scale=ENEMY_SCALING):
         super().__init__(image, scale)
         self.center_x
         self.center_y 
